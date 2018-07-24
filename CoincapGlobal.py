8433d0360c32bcd08381175090818103415d7b7b
Joseph Schell <JosephSchell510@gmail.com>
2018-07-23 20:22:35 -0700

    Formatted output to user.

--- CoincapGlobal.py
+++ CoincapGlobal.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from datetime import datetime
 
 globalURL = 'https://api.coinmarketcap.com/v2/global/'
 
@@ -15,5 +16,15 @@ last_updated = results['data']['last_updated']
 global_cap = results['data']['quotes']['USD']['total_market_cap']
 global_volume = results['data']['quotes']['USD']['total_volume_24h']
 
+active_currencies_string = '{:,}'.format(active_currencies)
+active_markets_string = '{:,}'.format(active_markets)
+global_cap_string = '{:,}'.format(global_cap)
+global_volume_string = '{:,}'.format(global_volume)
+last_updated_string = datetime.fromtimestamp(last_updated).strftime('%B %d, %Y at %I:%M%p')
+
 print()
-print('There are currently ' + str(active_currencies) + ' active cryptocurrencies.')
+print('There are currently ' + active_currencies_string + ' active cryptocurrecies and ' + active_markets_string + ' active markets.')
+print('The global cap of all cryptos is ' + global_cap_string + ' and the 24h global volume is ' + global_volume_string + '.')
+print('Bitcoin\'s total percentage of the global cap is ' + str(bitcoin_percentage) + '%.')
+print()
+print('This information was last updated on ' + last_updated_string + '.')
