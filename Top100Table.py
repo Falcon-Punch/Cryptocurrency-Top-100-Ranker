a214e98cdaf59b8746c18ad456592f52b321be22
Joseph Schell <JosephSchell510@gmail.com>
2018-07-23 20:56:21 -0700

    Added table row inserting and user choice to display menu again.

--- Top100Table.py
+++ Top100Table.py
@@ -29,9 +29,9 @@ while True:
     print('3 - Top 100 sorted by 24 hour volume')
     print('0 - Exit')
     print()
-    choice = input('Please choose an option (1-3): ')
+    choice = input('Please choose an option (0-3): ')
 
-    ticker_url = "https://api.coinmarketcap.com/v2/ticker/?structure=array&sort="
+    ticker_url = 'https://api.coinmarketcap.com/v2/ticker/?structure=array&sort='
 
     if choice == '1':
         ticker_url += 'rank'
@@ -49,6 +49,7 @@ while True:
     table = PrettyTable(['Rank', 'Asset', 'Price', 'Market Cap', 'Volume', '1h', '24h', '7d'])
 
     print()
+    # Grab data from Global API
     for currency in data:
         rank = currency['rank']
         name = currency['name']
@@ -61,4 +62,42 @@ while True:
         price = quotes['price']
         volume = quotes['volume_24h']
 
-        
+        # Account for None-type
+        if hour_change is not None:
+            if hour_change > 0:
+                hour_change = Back.GREEN + str(hour_change) + '%' + Style.RESET_ALL
+            else:
+                hour_change = Back.RED + str(hour_change) + '%' + Style.RESET_ALL
+
+        if day_change is not None:
+            if day_change > 0:
+                day_change = Back.GREEN + str(day_change) + '%' + Style.RESET_ALL
+            else:
+                day_change = Back.RED + str(day_change) + '%' + Style.RESET_ALL
+
+        if week_change is not None:
+            if week_change > 0:
+                week_change = Back.GREEN + str(week_change) + '%' + Style.RESET_ALL
+            else:
+                week_change = Back.RED + str(week_change) + '%' + Style.RESET_ALL
+
+        if volume is not None:
+            volume_string = '{:,}'.format(volume)
+
+        if market_cap is not None:
+            market_cap_string = '{:,}'.format(market_cap)
+
+        # Add rows to table
+        table.add_row([rank, name + ' ( ' + symbol + ')',
+                       '$' + str(price), '$' + str(market_cap),
+                       '$' + volume_string, str(hour_change),
+                       str(day_change), str(week_change)])
+
+    print()
+    print(table)
+    print()
+
+    choice = input('Display Table Menu Again? (y/n): ')
+
+    if choice == 'n':
+        break
