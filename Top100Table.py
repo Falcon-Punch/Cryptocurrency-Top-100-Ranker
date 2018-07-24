f70e603102bf4fc6577d316a52f268304bf66ba9
Joseph Schell <JosephSchell510@gmail.com>
2018-07-23 20:39:24 -0700

    Added menu loop for table. Has 3 options to sort table results.

--- Top100Table.py
+++ Top100Table.py
@@ -4,8 +4,10 @@ from datetime import datetime
 from prettytable import PrettyTable
 from colorama import Fore, Back, Style
 
+# Change currency type here
 convert = 'USD'
 
+# Coin Market Cap global API
 global_url = 'https://api.coinmarketcap.com/v2/global/?convert=' + convert
 
 request = requests.get(global_url)
@@ -14,3 +16,19 @@ data = results['data']
 
 global_cap = int(data['quotes'][convert]['total_market_cap'])
 global_cap_string = '{:,}'.format(global_cap)
+
+while True:
+
+    # Table Menu loop
+    print()
+    print('***Coin Market Cap Table Menu***')
+    print('The global market cap is $' + global_cap_string)
+    print()
+    print('1 - Top 100 sorted by rank')
+    print('2 - Top 100 sorted by 24 hour change')
+    print('3 - Top 100 sorted by 24 hour volume')
+    print('0 - Exit')
+    print()
+    choice = input('Please choose an option (1-3): ')
+
+    ticker_url = "https://api.coinmarketcap.com/v2/ticker/?structure=array&sort="
